87b6bcd7531fce96b9ace479c36bee87f7fdcfaa
dimitarp <dimitar.georgiev.popov@gmail.com>
2020-07-15 22:36:17 +0200

    ResourceBundles

--- demo-java/src/main/java/net/seitenbau/sdc2020/Demo.java
+++ demo-java/src/main/java/net/seitenbau/sdc2020/Demo.java
@@ -1,7 +1,12 @@
 package net.seitenbau.sdc2020;
 
+import java.util.ResourceBundle;
+
 public class Demo {
+  private static final ResourceBundle messages = ResourceBundle.getBundle("i18n.messages");
+
   public static void main(String[] args) {
-    System.out.println("Hallo SDC!");
+    var hello = messages.getString("hello.sdc");
+    System.out.println(hello);
   }
 }
