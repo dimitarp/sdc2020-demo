7a84aed5d8c8f11fe96a469bf8fa70a823fa8397
Dimitar Popov <dimitar.popov@seitenbau.com>
2020-07-16 11:58:18 +0200

    Add heavy class initialization.

--- demo-java/src/main/java/net/seitenbau/sdc2020/Demo.java
+++ demo-java/src/main/java/net/seitenbau/sdc2020/Demo.java
@@ -1,9 +1,17 @@
 package net.seitenbau.sdc2020;
 
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class Demo {
   private static final ResourceBundle messages = ResourceBundle.getBundle("i18n.messages");
+
+  static {
+    try {
+      TimeUnit.SECONDS.sleep(1L);
+    } catch (InterruptedException e) {
+    }
+  }
 
   public static void main(String[] args) {
     var hello = messages.getString("hello.sdc");
